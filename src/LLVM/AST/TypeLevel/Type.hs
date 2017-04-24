78a0d4e5f90ffb55a0207126a44ed6d703beb70d
Joachim Breitner <mail@joachim-breitner.de>
2017-04-24 19:45:03 -0400

    Consolidate KnownFoo classes into one kind-indexed class Known

--- src/LLVM/AST/TypeLevel/Type.hs
+++ src/LLVM/AST/TypeLevel/Type.hs
@@ -2,11 +2,14 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ExplicitForAll #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE TypeFamilyDependencies #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 {- |
 This modules contains a variant of the 'LLVM.AST.Type.Type' type, suitable for
@@ -50,77 +53,77 @@ data Type'
 
 -- TODO: Can we have one (kind) class to subsume the following repetition?
 
-class KnownType (t :: Type') where
-    typeVal :: Type
+type family Value k :: *
+class Known (t :: k)  where
+    val :: Value k
 
-class KnownTypes (t :: [Type']) where
-    typesVal :: [Type]
+type instance Value Type' = Type
+type instance Value [a] = [Value a]
+type instance Value AddrSpace' = AddrSpace
+type instance Value Name' = Name
+type instance Value FloatingPointFormat = FloatingPointFormat
+type instance Value Bool = Bool
+type instance Value Symbol = String
+type instance Value Nat = Integer
 
-class KnownAddrSpace (t :: AddrSpace') where
-    addrSpaceVal :: AddrSpace
+word32Val :: forall (n::Nat). Known n => Word32
+word32Val = fromIntegral (val @_ @n)
 
-class KnownName (t :: Name') where
-    nameVal :: Name
+word64Val :: forall (n::Nat). Known n => Word64
+word64Val = fromIntegral (val @_ @n)
 
-class KnownFloatingPointFormat (fpf :: FloatingPointFormat) where
-    floatingPointFormatVal :: FloatingPointFormat
+wordVal :: forall (n::Nat). Known n => Word
+wordVal = fromIntegral (val @_ @n)
 
-class KnownBool (b :: Bool) where
-    boolVal :: Bool
+instance Known VoidType' where
+    val = VoidType
+instance Known n => Known (IntegerType' n) where
+    val = IntegerType (word32Val @n)
+instance (Known t, Known as) => Known (PointerType' t as) where
+    val = PointerType (val @_ @t) (val @_ @as)
+instance (Known n, Known fpf) => Known (FloatingPointType' n fpf) where
+    val = FloatingPointType (word32Val @n) (val @_ @fpf)
+instance (Known t, Known ts, Known b) => Known (FunctionType' t ts b) where
+    val = FunctionType (val @_ @t) (val @_ @ts) (val @_ @b)
+instance (Known n, Known t) => Known (VectorType' n t) where
+    val = VectorType (word32Val @n) (val @_ @t)
+instance (Known b, Known ts) => Known (StructureType' b ts) where
+    val = StructureType (val @_ @b) (val @_ @ts)
+instance (Known n, Known t) => Known (ArrayType' n t) where
+    val = ArrayType (word64Val @n) (val @_ @t)
+instance Known n => Known (NamedTypeReference' n) where
+    val = NamedTypeReference (val @_ @n)
+instance Known MetadataType' where
+    val = MetadataType
+instance Known TokenType' where
+    val = TokenType
 
-word32Val :: forall n. KnownNat n => Word32
-word32Val = fromIntegral (natVal @n undefined)
+instance Known '[] where
+    val = []
+instance (Known t, Known tys) => Known (t:tys) where
+    val = (val @_ @t) : (val @_ @tys)
 
-word64Val :: forall n. KnownNat n => Word64
-word64Val = fromIntegral (natVal @n undefined)
+instance Known n => Known ('AddrSpace' n) where
+    val = AddrSpace (word32Val @n)
 
-wordVal :: forall n. KnownNat n => Word
-wordVal = fromIntegral (natVal @n undefined)
+instance Known s => Known ('Name' s) where
+    val = Name (val @_ @s)
+instance Known n => Known (UnName' n) where
+    val = UnName (wordVal @n)
 
-instance KnownType VoidType' where
-    typeVal = VoidType
-instance KnownNat n => KnownType (IntegerType' n) where
-    typeVal = IntegerType (word32Val @n)
-instance (KnownType t, KnownAddrSpace as) => KnownType (PointerType' t as) where
-    typeVal = PointerType (typeVal @t) (addrSpaceVal @as)
-instance (KnownNat n, KnownFloatingPointFormat fpf) => KnownType (FloatingPointType' n fpf) where
-    typeVal = FloatingPointType (word32Val @n) (floatingPointFormatVal @fpf)
-instance (KnownType t, KnownTypes ts, KnownBool b) => KnownType (FunctionType' t ts b) where
-    typeVal = FunctionType (typeVal @t) (typesVal @ts) (boolVal @b)
-instance (KnownNat n, KnownType t) => KnownType (VectorType' n t) where
-    typeVal = VectorType (word32Val @n) (typeVal @t)
-instance (KnownBool b, KnownTypes ts) => KnownType (StructureType' b ts) where
-    typeVal = StructureType (boolVal @b) (typesVal @ts)
-instance (KnownNat n, KnownType t) => KnownType (ArrayType' n t) where
-    typeVal = ArrayType (word64Val @n) (typeVal @t)
-instance KnownName n => KnownType (NamedTypeReference' n) where
-    typeVal = NamedTypeReference (nameVal @n)
-instance KnownType MetadataType' where
-    typeVal = MetadataType
-instance KnownType TokenType' where
-    typeVal = TokenType
+instance Known IEEE where
+    val = IEEE
+instance Known DoubleExtended where
+    val = DoubleExtended
+instance Known PairOfFloats where
+    val = PairOfFloats
 
-instance KnownTypes '[] where
-    typesVal = []
-instance (KnownType t, KnownTypes tys) => KnownTypes (t:tys) where
-    typesVal = (typeVal @t) : (typesVal @tys)
+instance Known True where
+    val = True
+instance Known False where
+    val = False
 
-instance KnownNat n => KnownAddrSpace ('AddrSpace' n) where
-    addrSpaceVal = AddrSpace (word32Val @n)
-
-instance KnownSymbol s => KnownName ('Name' s) where
-    nameVal = Name (symbolVal @s undefined)
-instance KnownNat n => KnownName (UnName' n) where
-    nameVal = UnName (wordVal @n)
-
-instance KnownFloatingPointFormat IEEE where
-    floatingPointFormatVal = IEEE
-instance KnownFloatingPointFormat DoubleExtended where
-    floatingPointFormatVal = DoubleExtended
-instance KnownFloatingPointFormat PairOfFloats where
-    floatingPointFormatVal = PairOfFloats
-
-instance KnownBool True where
-    boolVal = True
-instance KnownBool False where
-    boolVal = False
+instance KnownNat n => Known (n :: Nat) where
+    val = natVal @n undefined
+instance KnownSymbol s => Known (s :: Symbol) where
+    val = symbolVal @s undefined
